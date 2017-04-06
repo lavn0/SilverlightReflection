530d9ccd59a3ed2b5823f6b60acde850959a0fd3
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-04-06 21:56:04 +0900

    ユニットテスト整理

---
+++ SilverlightUnitTest/UnitTest2.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilverlightClassLibrary;
+
+namespace SilverlightUnitTest
+{
+	[TestClass]
+	public class UnitTest2
+	{
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			Publicer.Clear();
+		}
+
+		[TestMethod]
+		public void GetPropertyTestStr()
+		{
+			var obj = new Class1();
+			var result = Publicer.GetProperty<int>(obj.GetType(), "PropertyGet", obj);
+			Assert.AreEqual(3, result);
+		}
+
+		[TestMethod]
+		public void GetPropertyTestInfo()
+		{
+			var obj = new Class1();
+			var result = Publicer.GetProperty<int>(obj.GetType().GetProperty("PropertyGet", (BindingFlags)int.MaxValue), obj);
+			Assert.AreEqual(3, result);
+		}
+	}
+}
