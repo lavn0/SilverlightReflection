44a5ddb657c59c1c62c6c3de7748cc349a3c636b
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-06-15 22:44:04 +0900

    インスタンスクラスのstaticメンバのテスト追加

--- SilverlightClassLibrary/NonStaticClass.cs
+++ SilverlightClassLibrary/NonStaticClass.cs
@@ -2,8 +2,26 @@
 {
 	public class NonStaticClass
 	{
+		private static string StaticField = "static field";
+		private static string StaticProperty { get; set; } = "static property";
+
 		private string field = "field";
 		private string Property { get; set; } = "property";
+
+		private static bool StaticMethod()
+		{
+			return false;
+		}
+
+		private static string StaticMethod(int i)
+		{
+			return "static " + i.ToString("D2");
+		}
+
+		private static string StaticMethod(string item1, bool item2)
+		{
+			return "static " + item1 + item2;
+		}
 
 		private bool Method()
 		{
--- SilverlightUnitTest/PublicerTest.cs
+++ SilverlightUnitTest/PublicerTest.cs
@@ -82,5 +82,67 @@ namespace SilverlightUnitTest
 			var result3 = Publicer.InvokeMethod<string>(typeof(NonStaticClass), "Method", new[] { typeof(string), typeof(bool) }, obj, "hoge", true);
 			Assert.AreEqual("hogeTrue", result3);
 		}
+
+		[TestMethod]
+		public void GetStaticFieldTest()
+		{
+			var result = Publicer.GetField<string>(typeof(NonStaticClass), "StaticField", null);
+			Assert.AreEqual("static field", result);
+		}
+
+		[TestMethod]
+		public void SetStaticFieldTest()
+		{
+			Publicer.SetField(typeof(NonStaticClass), "StaticField", null, "field set value");
+			var result = Publicer.GetField<string>(typeof(NonStaticClass), "StaticField", null);
+			Assert.AreEqual("field set value", result);
+		}
+
+		[TestMethod]
+		public void GetStaticPropertyTest()
+		{
+			var result = Publicer.GetProperty<string>(typeof(NonStaticClass), "StaticProperty", null);
+			Assert.AreEqual("static property", result);
+		}
+
+		[TestMethod]
+		public void SetStaticPropertyTest()
+		{
+			Publicer.SetProperty(typeof(NonStaticClass), "StaticProperty", null, "property set value");
+			var result = Publicer.GetProperty<string>(typeof(NonStaticClass), "StaticProperty", null);
+			Assert.AreEqual("property set value", result);
+		}
+
+		[TestMethod]
+		public void InvokeStaticMethodTest1()
+		{
+			var result = Publicer.InvokeMethod<bool>(typeof(NonStaticClass), "StaticMethod", new Type[0], null);
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod]
+		public void InvokeStaticMethodTest2()
+		{
+			var result = Publicer.InvokeMethod<string>(typeof(NonStaticClass), "StaticMethod", new[] { typeof(int) }, null, 5);
+			Assert.AreEqual("static 05", result);
+		}
+
+		[TestMethod]
+		public void InvokeStaticMethodTest3()
+		{
+			var result = Publicer.InvokeMethod<string>(typeof(NonStaticClass), "StaticMethod", new[] { typeof(string), typeof(bool) }, null, "hoge", true);
+			Assert.AreEqual("static hogeTrue", result);
+		}
+
+		[TestMethod]
+		public void InvokeStaticMethodTest123()
+		{
+			var result1 = Publicer.InvokeMethod<bool>(typeof(NonStaticClass), "StaticMethod", new Type[0], null);
+			Assert.AreEqual(false, result1);
+			var result2 = Publicer.InvokeMethod<string>(typeof(NonStaticClass), "StaticMethod", new[] { typeof(int) }, null, 5);
+			Assert.AreEqual("static 05", result2);
+			var result3 = Publicer.InvokeMethod<string>(typeof(NonStaticClass), "StaticMethod", new[] { typeof(string), typeof(bool) }, null, "hoge", true);
+			Assert.AreEqual("static hogeTrue", result3);
+		}
 	}
 }
